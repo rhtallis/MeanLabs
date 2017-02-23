20133caafbe85601e4032dee9086c42ecc6e9aa7
Tallis Hobbs <rhtallis@gmail.com>
2017-02-22 18:39:41 -0800

    Lab 4 first commit
    Starting Lab 4

---
+++ StudentsAPI/server.js
@@ -0,0 +1,30 @@
+var express = required('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var students = require('./routes/countries');
+
+var port = 3000;
+var app = express();
+
+// Generate View Engine
+var viewEngine = require('ejs-locals');
+app.engine('ejs', viewEngine);
+app.set('view engine', 'ejs');
+
+// set folder for static content
+app.use(express.static(path.join(__dirname, 'client')));
+
+// body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// define the routings
+app.use('/', index);
+app.use('/api', students);
+
+//  apply the listener
+app.listen(port, function(){
+    console.log('server started on port')
+});
